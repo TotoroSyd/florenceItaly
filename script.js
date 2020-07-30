6799367387ba8c0ff1c2f78241f4c133d54f8c0b
TototoSyd <61923601+TotoroSyd@users.noreply.github.com>
2020-07-30 17:11:07 +1000

    first commit
    MVP

---
+++ script.js
@@ -0,0 +1,14 @@
+// const slice_switch = document.querySelector(
+//   '.theme-switch input[type="checkbox"]'
+// );
+
+// slice_switch.addEventListener("checked", function () {});
+
+// const toggleSwitch = document.querySelector("theme-switch");
+function darkMode() {
+  document.body.classList.add("dark");
+}
+
+function lightMode() {
+  document.body.classList.remove("dark");
+}
